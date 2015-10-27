1587e9ac81115843cfec9a3cfcd16cfee497eebe
rakesha1989 <rakesh.qubik@gmail.com>
2015-10-27 15:36:08 +0530

    add comment feature by user

--- vendor/faker.rb
+++ vendor/faker.rb
@@ -5,3 +5,9 @@ post.content = Faker::Lorem.paragraph(10)
 post.user_id = User.order("RANDOM()").first.id
 post.save	
 end
+
+20.times do
+comment = Comment.new
+comment.user_id = User.order("RANDOM()").first.id
+comment.save
+end
